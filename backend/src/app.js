a0bf4a5acb91d4ea8e32de97027d2e4d48cf6bc5
Admin <riturajjha938@gmail.com>
2026-05-17 22:06:44 +0530

    feat: Add foundational data models and CRUD API for Conflict tracking

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const conflictRoutes = require('./routes/conflictRoutes');
+
+app.use('/conflicts', conflictRoutes);
+
 // Default Route
 app.get('/', (req, res) => {
   res.send('API is running...');
