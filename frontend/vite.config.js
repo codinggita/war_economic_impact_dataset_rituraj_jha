0621e14f1cbbc3eca5469a5e929932fdddad0e66
Admin <riturajjha938@gmail.com>
2026-06-18 10:37:51 +0530

    fix: add COOP headers to allow google auth popups

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,11 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
